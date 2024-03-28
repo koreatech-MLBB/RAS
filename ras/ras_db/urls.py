96936307d59a99a6abad9faf4fbf5b07dedcdc4e
cux-maks <volcano020716@gmail.com>
2024-03-28 17:01:11 +0900

    add: ras_db/index

--- ras/ras_db/urls.py
+++ ras/ras_db/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.index),
+    path('hello', views.hello)
 ]
+g
--- ras/ras_db/views.py
+++ ras/ras_db/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse("ras_db index")
 
 def hello(request):
     return HttpResponse("Hello!! This is ras_db!!")
-
